f72efb48b76357ac3f12514276522c3d79f53009
Ephraim Alonso Puerto <efrainpuerto@gmail.com>
2020-06-08 17:08:33 -0500

    Added DataTool

---
+++ DataTool.java
@@ -0,0 +1,54 @@
+package maths;
+
+public abstract class DataTool {
+	public static double interpolate(double[] range, double x, double[] data) {
+		int i = 0;
+		boolean exit = false;
+		while(i < range.length - 1 && exit == false) {
+			if(x <= range[i + 1]) {
+				exit = true;
+			}
+			i++;
+		}
+		double ratio = (x - range[i - 1]) / (range[i] - range[i - 1]);
+		double value1 = data[i];
+		double value2 = data[i - 1];
+		return (data[i] - data[i - 1]) * ratio + data[i - 1];
+	}
+	
+	public static double interpolate2D(double[] rangeX, double x, double[] rangeY, double y, double[][] data) {
+		double result;
+		int i = 0;
+		boolean exit = false;
+		while(i <= rangeY.length - 1 && exit == false) {
+			if (y <= rangeY[i + 1]) {
+				exit = true;
+			}
+			i++;
+		}
+		double ratio, ymin, ymax;
+		ratio = (y - rangeY[i - 1]) / (rangeY[i] - rangeY[i - 1]);
+		ymin = interpolate(rangeX, x, data[i - 1]);
+		ymax = interpolate(rangeX, x, data[i]);
+		result = (ymax - ymin) * ratio + ymin;
+		return result;
+	}
+	
+	public static double interpolate3D(double rangeX[], double x, double rangeY[], double y, double rangeZ[], double z, double[][][] matrix) {
+		double result;
+		int i = 0;
+		boolean exit = false;
+		while(i < rangeZ.length  - 1 && exit == false) {
+			if(z < rangeZ[i + 1]) {
+				exit = true;
+			}
+			i++;
+		}
+		double ratio, zmin, zmax;
+		ratio = (x - rangeX[i - 1]) / (rangeX[i] - rangeX[i - 1]);
+		zmin = interpolate2D(rangeX, x, rangeY, y, matrix[i - 1]);
+		zmax = interpolate2D(rangeX, x, rangeY, y, matrix[i]);
+		result = (zmax - zmin) * ratio + zmin;
+		return result;
+	}
+}
